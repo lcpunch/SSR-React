892fa3b2777fe053a7401768e8ed33ef196d81e0
lucas <lucas@umaknow.com>
2020-06-15 10:26:39 -0400

    test

--- webpack.server.js
+++ webpack.server.js
@@ -7,6 +7,7 @@ module.exports = {
     // Tell webpack the root file of our server application
     entry: './src/index.js',
 
+
     // Tell webpack where to put the output file that is generated 
     output: {
         filename: 'bundle.js',
